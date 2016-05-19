Fixed handling of gear ratios other than 2; improved quality per rotation
Signed-off-by: Samuel Ettinger <Samuel Ettinger>

animate.py
@@ -5,7 +5,7 @@ THIS PART IS LESS GOOD THAN THE OTHER PART
 @author: Sam
 """
 
-import Image
+from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -23,15 +23,19 @@ def outputGearImage(image, coords, scale, ratio):
 def colorfulArray(inGear, outGear, inScale, ratio):
     image = 255*np.ones([1500,1500,3], 'uint8')
     for (x,y) in inGear:
-        row = int(np.floor((y+ratio)*ratio/(2.*inScale)))
-        col = int(np.floor((x+ratio)*ratio/(2.*inScale)))
+#        row = int(np.floor((y+ratio)*ratio/(2.*inScale)))
+#        col = int(np.floor((x+ratio)*ratio/(2.*inScale)))
+        row = int(100*(y+2))
+        col = int(100*(x+2))
         image[row][col][0] = 0.0
         image[row][col][1] = 0.0
     for (x,y) in outGear:
 #        row = int(np.floor((y+ratio)*ratio/(2.*inScale)))
 #        col = int(np.floor((x+ratio)*ratio/(2.*inScale)))
-        row = np.floor((y+ratio)*ratio/(2.*inScale/ratio))
-        col = np.floor((x+ratio)*ratio/(2.*inScale/ratio))
+#        row = np.floor((y+ratio)*ratio/(2.*inScale/ratio))
+#        col = np.floor((x+ratio)*ratio/(2.*inScale/ratio))
+        row = int(100*(y+2))
+        col = int(100*(x+2))
         image[row][col][2] = 0.0
         image[row][col][1] = 0.0
     return image
@@ -44,7 +48,6 @@ def animate(inputGear, outputGear, ratio, overlap):
     outRows = len(outputGear)
     outCols = outRows # Output gear should be square
     size = inRows*(inRows>=inCols) + inCols*(inCols>inRows) # Get largest dimension
-    #overlap = round(overlap*size/2.0)
     
     offset = (ratio+1-overlap, 0)
     inCoords, inScale = getBlackPixels(inputGear, offset)
@@ -52,13 +55,20 @@ def animate(inputGear, outputGear, ratio, overlap):
     outCoords = [(ratio*x, ratio*y) for (x,y) in outCoords]
     
     frames = 60
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     for frame in range(frames):
         theta = 2*np.pi*frame/frames
         phi = -theta*ratio
         inGearRot = rotatePts(inCoords, offset, phi)
         outGearRot = rotatePts(outCoords, (0,0), theta)
-        # allCoords = inGearRot + outGearRot
-        image = colorfulArray(inGearRot, outGearRot, inScale, ratio)
-        #plt.imshow(image)
+        inX = [x for (x,y) in inGearRot]
+        inY = [y for (x,y) in inGearRot]
+        outX = [x for (x,y) in outGearRot]
+        outY = [y for (x,y) in outGearRot]
+        ax.clear()
+        ax.scatter(inX, inY, marker='.', c='r')
+        ax.scatter(outX, outY, marker='.', c='b')
         #time.sleep(1)
+        image = colorfulArray(inGearRot, outGearRot, inScale, ratio)
         writeOutputGear(image, str(frame)+'.png')

main.py
@@ -3,18 +3,41 @@
 Updated Gear Math Python
 
 @author: Sam Ettinger
+EttingerSam@gmail.com
+May 19, 2016
+
+TODO:
+Add a GUI?
 """
 
-'''
-TODO:
-* Add support for non-integer gear ratios (e.g. 3:2, 5:3)
-'''
+''''''''''''''''''''''''''
+'''IMPORTANT PARAMETERS'''
+''''''''''''''''''''''''''
+# Define the gear ratio, which is the number of rotations the input gear com-
+# pletes for each one rotation of the output gear. Must be a positive integer.
+gearRatio = 2
+
+# Define the gear overlap. This should be a decimal value between 0 and 1.
+# This is analogous to tooth size on a spur gear, with 0.0 being "no teeth"
+# and 1.0 being "pretty big teeth."
+gearOverlap = 1.0
+
+# Define the number of computation steps. This is how many tiny rotations the
+# program performs to compute the final bitmap. Higher numbers produce better
+# gear profiles, with a tradeoff in speed. Must be an integer.
+computationSteps = 1000
+
+''''''''''''''''''''''''''
+'''   END PARAMETERS   '''
+''''''''''''''''''''''''''
 
 import Image
 import numpy as np
+import tkFileDialog
 
-def loadGearImage(filename):
+def loadGearImage():
     '''Loads image, converts to a B/W array.'''
+    filename = tkFileDialog.askopenfilename()
     img = Image.open(filename)
     img.load()
     data = np.asarray(img)
@@ -36,14 +59,14 @@ def getBlackPixels(image, offset):
                 x = scale*(col - (cols-1)/2.) + offset[0]
                 y = scale*(row - (rows-1)/2.) + offset[1]
                 coords += [(x, y)]
-    return coords, scale
+    return coords, size
 
-def outputGearImage(image, coords, scale, ratio):
+def outputGearImage(image, coords, size, ratio):
     '''Draws coordinates as pixels on image'''
     newImage = image
     for (x,y) in coords:
-        row = np.floor((y+ratio)*ratio/(2.*scale))
-        col = np.floor((x+ratio)*ratio/(2.*scale))
+        row = np.floor((y+ratio)*size/(2*ratio))
+        col = np.floor((x+ratio)*size/(2*ratio))
         newImage[row][col] = 255.0
     return newImage
     
@@ -76,16 +99,14 @@ def outputCleanup(image):
                 newImage[row][col] = 255.0
     return newImage
 
-def doThings(filename, ratio, overlap, steps):
-    inputGear = loadGearImage(filename)
+def doThings(ratio=gearRatio, overlap=gearOverlap, steps=computationSteps):
+    inputGear = loadGearImage()
     offset = (ratio+1-overlap, 0)
-    inputCoords, imageScale = getBlackPixels(inputGear, offset)
-    inputImageSize = int(np.round(2./imageScale))
-    outputImageSize = int(np.ceil(inputImageSize*ratio))
-#    outputCoords = []
+    inputCoords, inputImageSize = getBlackPixels(inputGear, offset)
+    outputImageSize = inputImageSize*ratio
     outputGear = np.zeros([outputImageSize, outputImageSize])
-    theta = 2*np.pi*ratio/steps    # TODO: Change these two lines in order...
-    phi = 2*np.pi/steps            # ...to accommodate non-integer gear ratios
+    theta = 2*np.pi/steps
+    phi = 2*np.pi/(steps*ratio)
     # Rotation math
     for step in range(steps):
         coords = rotatePts(inputCoords, offset, theta*step)
@@ -93,14 +114,21 @@ def doThings(filename, ratio, overlap, steps):
         for coord in coords:
             if dist(*coord)<(ratio+1-overlap):
                 addPoints += [coord]
-        # Rotate the points that will contribute to the output gear's profile
-        addPoints = rotatePts(addPoints, (0,0), phi*step)
-        # Convert those points back into pixels for output gear
-        outputGear = outputGearImage(outputGear, addPoints, imageScale, ratio)
-        print step # Debug
+        # Rotate the points that contribute to the output gear's profile
+        for extraRotation in range(ratio):
+            rotateBy = phi*step + 2*np.pi*extraRotation/ratio
+            addPointsRot = rotatePts(addPoints, (0,0), rotateBy)
+            # Convert those points into pixels, draw on output gear
+            outputGear = outputGearImage(outputGear, addPointsRot, outputImageSize, ratio)
+        print('Progress: {}/{}'.format(step, steps)) # Debug
     # Clean up image
     outputGear = outputCleanup(outputGear)
     # Should also make little marks for centroids and distances
     # Animate?
     # Save image
-    writeOutputGear(outputGear, 'agear.png')
+    outFilename = tkFileDialog.asksaveasfilename(defaultextension='.png')
+    writeOutputGear(outputGear, outFilename)
+    #return inputGear, outputGear
+    
+if __name__ == '__main__':
+    doThings()