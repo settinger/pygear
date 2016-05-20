0085ebb613143cfe0257e61ee408707bd2633daf
Samuel Ettinger <Samuel Ettinger>
2016-05-20 23:09:44 +0800

    Added crossbar generation, updated readme

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def outputGearImage(image, coords, size, ratio):
     '''Draws coordinates as pixels on image'''
     newImage = image
     for (x,y) in coords:
-        row = np.floor((y+ratio)*size/(2*ratio))
-        col = np.floor((x+ratio)*size/(2*ratio))
+        row = int(np.floor((y+ratio)*size/(2*ratio)))
+        col = int(np.floor((x+ratio)*size/(2*ratio)))
         newImage[row][col] = 255.0
     return newImage
     
@@ -89,7 +89,8 @@ def dist(x, y):
     return np.sqrt(x*x+y*y)
 
 def outputCleanup(image):
-    '''Remove the 'halo' around output image; maybe apply median filter?'''
+    '''Remove the 'halo' around output image; adds a mark indicating the center
+    TODO: maybe apply median filter?'''
     newImage = image
     size = len(image) # Should be same number of rows/columns
     radius = size/2.
@@ -97,7 +98,43 @@ def outputCleanup(image):
         for col in range(size):
             if dist(row-radius, col-radius) >= radius-.5:
                 newImage[row][col] = 255.0
+    # Mark the center
+    markRadius = np.max([2., size/200.]) # How big to make the mark
+    for i in range(50):
+        theta = i*2*np.pi/50
+        x = int(np.round(radius + markRadius*np.cos(theta)))
+        y = int(np.round(radius + markRadius*np.sin(theta)))
+        newImage[y][x] = 255.0
     return newImage
+
+def drawCrossbar(distance):
+    distance = int(distance) # just in case
+    '''Draws the image of the crossbar that holds the two gear axles'''
+    # Size of the image:
+    height = int(np.round(distance/6.))
+    width = int(np.ceil(distance*7./6))
+    # Coordinates of the axle holes' centers:
+    radius = height/2. - 0.5
+    holeOne = (radius, radius)
+    holeTwo = (distance+radius, radius)
+    # Draw the crossbar:
+    crossbarImage = 255.0*np.ones((height, width))
+    crossbarImage[(0, height-1), int(np.ceil(holeOne[0])):int(np.floor(holeTwo[0])+1)] = 0.
+    for i in range(distance):
+        theta = np.pi*i/distance - np.pi/2
+        rows = (int(np.round(holeOne[1] - radius*np.sin(theta))), int(np.round(holeTwo[1] + radius*np.sin(theta))))
+        cols = (int(np.round(holeOne[0] - radius*np.cos(theta))), int(np.round(holeTwo[0] + radius*np.cos(theta))))
+        crossbarImage[rows, cols] = 0.0
+    # Draw crossbar holes
+    markRadius = np.max([2., distance/200.]) # How big to make the mark
+    for i in range(50):
+        theta = i*2*np.pi/50
+        x = int(np.round(holeOne[0] + markRadius*np.cos(theta)))
+        y = int(np.round(holeOne[1] + markRadius*np.sin(theta)))
+        crossbarImage[y][x] = 0.
+        crossbarImage[y][x+distance] = 0.
+    return crossbarImage
+        
 
 def doThings(ratio=gearRatio, overlap=gearOverlap, steps=computationSteps):
     inputGear = loadGearImage()
@@ -126,8 +163,12 @@ def doThings(ratio=gearRatio, overlap=gearOverlap, steps=computationSteps):
     # Should also make little marks for centroids and distances
     # Animate?
     # Save image
-    outFilename = tkFileDialog.asksaveasfilename(defaultextension='.png')
+    outFilename = tkFileDialog.asksaveasfilename(defaultextension='.png', initialfile='gear_output')
     writeOutputGear(outputGear, outFilename)
+    # save the crossbar image too
+    crossbar = drawCrossbar(inputImageSize*(ratio+1-overlap)/2)
+    outFilename = tkFileDialog.asksaveasfilename(defaultextension='.png', initialfile='crossbar')
+    writeOutputGear(crossbar, outFilename)
     #return inputGear, outputGear
     
 if __name__ == '__main__':
